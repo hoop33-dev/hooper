707765a70b695fc6744bcb4346594c13d1f896c7
Joe Wuthrich <joewuthrich@Joes-MacBook-Pro.local>
2026-03-31 17:22:00 +1300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, View } from "react-native";
+
+export default function App() {
+  return <View style={styles.root} />;
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: "#0F0F1A",
+  },
+});

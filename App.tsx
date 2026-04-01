48669bce7a659871ed5246cc0f44af2d1ce6dc21
Joe Wuthrich <joewuthrich@Joes-MacBook-Pro.local>
2026-04-01 16:57:14 +1300

    Added nativewind

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
-import { StyleSheet, View } from "react-native";
+import "./global.css";
+
+import { View, Text } from "react-native";
 
 export default function App() {
-  return <View style={styles.root} />;
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Hooper!
+      </Text>
+    </View>
+  );
 }
-
-const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    backgroundColor: "#0F0F1A",
-  },
-});

49aedb8645fa0b5d361ec0c6346fc8a9dc1817f5
Hoop33 <dev@hoop33.co.nz>
2026-08-16 11:51:31 +1200

    Updated create exercise button (#75)
    Co-authored-by: Joe Wuthrich <joerwuthrich@gmail.com>

--- apps/web/src/components/portal/exercises/ExerciseLibraryShell.tsx
+++ apps/web/src/components/portal/exercises/ExerciseLibraryShell.tsx
@@ -278,7 +278,7 @@ function LibraryToolbar({
           <svg className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor">
             <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
           </svg>
-          Create exercise
+          Create
         </PortalButton>
       </div>
     </div>
@@ -555,7 +555,7 @@ function EmptyState({
       </div>
       {!hasFilters && (
         <PortalButton variant="primary" onClick={onCreateClick}>
-          Create exercise
+          Create
         </PortalButton>
       )}
     </div>
